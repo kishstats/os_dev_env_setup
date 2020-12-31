15de011eb275bb1a5d5c710238f3a96dfa5bef48
Brett Kishkis <brett.kishkis@scalefunder.com>
2020-12-30 16:39:48 -0800

    ubuntu server 20.04

---
+++ linux/ubuntu-server/20-04/ubuntu-20.04-setup.sh
@@ -0,0 +1,32 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get install vim curl git tree ntp htop traceroute mlocate net-tools -y
+
+# python
+sudo apt-get install python-pip -y
+sudo apt-get install python3-pip -y
+
+# php
+sudo apt-get install php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-imagick php-mbstring php-curl php-xml php-pear php-bcmath php-opcache php-readline -y
+
+sudo systemctl enable php-fpm
+sudo systemctl start php-fpm
+
+# nodejs
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm install 14.5.1
+nvm use 14.5.1
+npm install --global gulp karma-cli
+
+# mysql
+sudo apt-get install mysql-server-5.7 -y
+sudo systemctl enable mysql
+sudo systemctl start mysql
+
+# nginx
+sudo apt-get install nginx -y
+sudo chown www-data:www-data /usr/share/nginx/html/ -R
+sudo systemctl enable nginx
+sudo systemctl start nginx
